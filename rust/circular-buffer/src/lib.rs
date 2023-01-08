e35976c8f62aa7f8c394015a8fde1bcec9579df6
ruiProg <ruifilipeosorio17@hotmail.com>
2023-01-08 17:53:44 +0000

    circular buffer Rust exercise

--- rust/circular-buffer/src/lib.rs
+++ rust/circular-buffer/src/lib.rs
@@ -1,7 +1,10 @@
+use std::{cmp::Ordering, mem::swap};
+
 pub struct CircularBuffer<T> {
-    // We fake using T here, so the compiler does not complain that
-    // "parameter `T` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<T>,
+    data: Vec<T>,
+    read_index: usize,
+    write_index: usize,
+    size: usize,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -10,30 +13,59 @@ pub enum Error {
     FullBuffer,
 }
 
-impl<T> CircularBuffer<T> {
+impl<T> CircularBuffer<T>
+where
+    T: Default,
+{
     pub fn new(capacity: usize) -> Self {
-        unimplemented!(
-            "Construct a new CircularBuffer with the capacity to hold {}.",
-            match capacity {
-                1 => "1 element".to_string(),
-                _ => format!("{} elements", capacity),
-            }
-        );
+        let mut data = Vec::new();
+        data.resize_with(capacity, T::default);
+
+        Self {
+            data,
+            read_index: 0,
+            write_index: 0,
+            size: 0,
+        }
     }
 
-    pub fn write(&mut self, _element: T) -> Result<(), Error> {
-        unimplemented!("Write the passed element to the CircularBuffer or return FullBuffer error if CircularBuffer is full.");
+    pub fn write(&mut self, element: T) -> Result<(), Error> {
+        match self.size.cmp(&self.data.len()) {
+            Ordering::Equal => Err(Error::FullBuffer),
+            _ => {
+                self.write_impl(element);
+                Ok(())
+            }
+        }
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        unimplemented!("Read the oldest element from the CircularBuffer or return EmptyBuffer error if CircularBuffer is empty.");
+        match self.size {
+            0 => Err(Error::EmptyBuffer),
+            _ => {
+                let mut result = T::default();
+                swap(&mut result, &mut self.data[self.read_index]);
+                self.read_index = (self.read_index + 1) % self.data.len();
+                self.size -= 1;
+                Ok(result)
+            }
+        }
     }
 
     pub fn clear(&mut self) {
-        unimplemented!("Clear the CircularBuffer.");
+        *self = Self::new(self.data.len());
     }
 
-    pub fn overwrite(&mut self, _element: T) {
-        unimplemented!("Write the passed element to the CircularBuffer, overwriting the existing elements if CircularBuffer is full.");
+    pub fn overwrite(&mut self, element: T) {
+        if self.size == self.data.len() {
+            self.read_index += 1;
+        }
+        self.write_impl(element);
+    }
+
+    fn write_impl(&mut self, element: T) {
+        self.data[self.write_index] = element;
+        self.write_index = (self.write_index + 1) % self.data.len();
+        self.size += 1;
     }
 }
